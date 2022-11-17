b74023fce4aa7be3ddcea45e478f6d2755b87821
Alim Charaniya <alim.charaniya@sjsu.edu>
2022-11-17 11:53:58 -0500

    removed course related events

--- lib/services/mixpanel.dart
+++ lib/services/mixpanel.dart
@@ -5,8 +5,6 @@ class Mixpanell {
 }
 
 Future getInstance() async {
-  // Mixpanell.mixpanel = await Mixpanel.init("bc1020e51bd5d65cb512f6e1906cf6c4",
-  //     optOutTrackingDefault: false); // development mixpanel token
   Mixpanell.mixpanel = await Mixpanel.init(
     "d0b9a45e61612a70e7a3f6bb8396a918",
     optOutTrackingDefault: false,
@@ -14,29 +12,18 @@ Future getInstance() async {
   ); // production mixpanel token
 }
 
+// TODO rename this to mixpanelIdentify
 Future mixPanelStart(String id, String _name, String _email) async {
   String distinctID = id; //await Mixpanell.mixpanel!.getDistinctId();
   Mixpanell.mixpanel!.identify(
     distinctID,
   );
-  // Mixpanell.mixpanel!.alias(
-  //   "New user",
-  //   distinctID,
-  // );
-
-  Mixpanell.mixpanel!.getPeople().set(
-        "\$name",
-        _name,
-      );
-  Mixpanell.mixpanel!.getPeople().set(
-        "\$email",
-        _email,
-      );
-  await Mixpanell.mixpanel!.flush();
 }
 
+// TODO rename to mixpanelTrack
 Future mixpanelTracking(String eventname,
-    [Map<String, dynamic> properties = const {}]) async {
+  [Map<String, dynamic> properties = const {}]) 
+  async {
   Mixpanell.mixpanel!.track(
     eventname,
     properties: properties,
